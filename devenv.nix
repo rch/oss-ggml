0ef2fa75037036b852be31841093b58f913b1f39
Ryan Hill <rch+git@zndx.org>
2023-07-25 12:43:19 -0600

    devenv

---
+++ devenv.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.cmake
+    pkgs.git
+    pkgs.python310
+    pkgs.python310Packages.accelerate
+    pkgs.python310Packages.numpy
+    pkgs.python310Packages.sentencepiece
+    pkgs.python310Packages.torch
+    pkgs.python310Packages.torchaudio
+    pkgs.python310Packages.torchvision
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
